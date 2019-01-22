8614c3136f2c87ddcd9f80a05e0cbddc3ad82954
David Jones <dgjones@gmail.com>
2019-01-21 23:13:43 -0800

    demo base

---
+++ tests/todo_0_spec.js
@@ -0,0 +1,28 @@
+const should = require('chai').should()
+const puppeteer = require('puppeteer')
+// const { percySnapshot } = require('@percy/puppeteer')
+
+const TEST_URL = "http://localhost:8000"
+
+describe('Index', function() {
+  let browser = null
+  let page = null
+
+  beforeEach(async function() {
+    browser = await puppeteer.launch({
+      headless: true,
+      timeout: 10000,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+    page = await browser.newPage()
+  })
+
+  afterEach(function() { browser.close() })
+
+  it('Loads', async function() {
+    await page.goto(TEST_URL)
+    const mainContainer = await page.$('section.todoapp')
+    should.exist(mainContainer)
+    // await percySnapshot(page, this.test.fullTitle())
+  })
+})
--- tests/todo_1_spec.js
+++ tests/todo_1_spec.js
@@ -1,39 +1,23 @@
 const should = require('chai').should()
 const puppeteer = require('puppeteer')
-const { percySnapshot } = require('@percy/puppeteer')
+// const { percySnapshot } = require('@percy/puppeteer')
 
 const TEST_URL = "http://localhost:8000"
 
-describe('TodoMVC', function() {
-
+describe('Todos', function() {
   let browser = null
   let page = null
+
   beforeEach(async function() {
-    // Create a new Puppeteer browser instace for each test case
     browser = await puppeteer.launch({
       headless: true,
-      timeout: 10000
+      timeout: 10000,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
     page = await browser.newPage()
   })
 
-  afterEach(function() {
-    // Close the Puppeteer browser instance.
-    browser.close()
-  })
-
-  it('Loads the app', async function() {
-    await page.goto(TEST_URL)
-    const mainContainer = await page.$('section.todoapp')
-    should.exist(mainContainer)
-    await percySnapshot(page, this.test.fullTitle())
-  })
-
-  it('With no todos, hides main section', async function() {
-    await page.goto(TEST_URL)
-    const hiddenMainSection = await page.$(".main[style*='display:none']")
-    should.exist(hiddenMainSection)
-  })
+  afterEach(function() { browser.close() })
 
   it('Accepts a new todo', async function() {
     await page.goto(TEST_URL)
@@ -41,7 +25,7 @@ describe('TodoMVC', function() {
     await page.keyboard.press('Enter')
     const todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length)
     todoCount.should.eq(1)
-    await percySnapshot(page, 'Snapshot with new todo', {widths: [300]})
+    // await percySnapshot(page, 'Snapshot with new todo')
   })
 
   it('Lets you check off a todo', async function() {
@@ -52,10 +36,9 @@ describe('TodoMVC', function() {
     let itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent)
     itemsLeft.should.eq('1 item left')
 
-
     await page.click('input.toggle')
     itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent)
     itemsLeft.should.eq('0 items left')
-    await percySnapshot(page, this.test.fullTitle(), {widths: [768, 992, 1200]})
+    // await percySnapshot(page, this.test.fullTitle(), {widths: [768, 992, 1200]})
   })
 })
