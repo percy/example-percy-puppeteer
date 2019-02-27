d06cfbdfe13164fac93688f52ce2a6effedca6fb
Robert DeLuca <robertdeluca19@gmail.com>
2019-02-27 10:28:17 -0600

    Use HTTP server over express to match other examples

--- run-tests.js
+++ run-tests.js
@@ -1,34 +1,25 @@
-const express = require('express')
-const app = express()
+const httpServer = require('http-server');
 const port = process.env.PORT_NUMBER || 8000;
 const spawn = require('child_process').spawn;
-const platform = require("os").platform()
+const platform = require("os").platform();
 
 // We need to change the command path based on the platform they're using
 const cmd = /^win/.test(platform)
-  ? `${process.cwd()}\\node_modules\\.bin\\mocha.cmd`
-  : `mocha`
+      ? `${process.cwd()}\\node_modules\\.bin\\mocha.cmd`
+      : `mocha`;
 
-app.use(express.static(__dirname))
+const server = httpServer.createServer()
+const PORT = 8000
 
-app.get('/', (request, response) => {
-  response.render('index.html')
-})
+server.listen(port)
+console.log(`Server is listening on http://localhost:${port}`)
 
-const server = app.listen(port, async (err) => {
-  if (err) {
-    return console.log('Could not serve application', err)
-  }
+const tests = spawn(cmd, ['tests'], {
+  stdio: "inherit",
+  windowsVerbatimArguments: true,
+});
 
-  console.log(`Server is listening on http://localhost:${port}`)
-
-  const tests = spawn(cmd, ['tests'], {
-    stdio: "inherit",
-    windowsVerbatimArguments: true,
-  });
-
-  tests.on('close', () => {
-    console.log(`Tests finished! Closing server http://localhost:${port}`);
-    server.close();
-  });
-})
+tests.on('close', () => {
+  console.log(`Tests finished! Closing server http://localhost:${port}`);
+  server.close();
+});
