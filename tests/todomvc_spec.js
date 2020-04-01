ff51d5e9f7cabf07024802d0bf1d3065a178333c
Robert DeLuca <robertdeluca19@gmail.com>
2020-04-01 10:29:12 -0500

    test: Only launch a browser once in tests
    It's a bit intense to relaunch a browser before each test case. Lets just create
    new tabs instead

--- tests/todomvc_spec.js
+++ tests/todomvc_spec.js
@@ -1,96 +1,91 @@
 const httpServer = require('http-server');
-const should = require('chai').should()
-const puppeteer = require('puppeteer')
-const { percySnapshot } = require('@percy/puppeteer')
-const platform = require("os").platform()
+const should = require('chai').should();
+const puppeteer = require('puppeteer');
+const { percySnapshot } = require('@percy/puppeteer');
+const platform = require('os').platform();
 // We need to change the args passed to puppeteer based on the platform they're using
-const puppeteerArgs = /^win/.test(platform) ? [] : [ '--single-process' ]
+const puppeteerArgs = /^win/.test(platform) ? [] : ['--single-process'];
 const PORT = process.env.PORT_NUMBER || 8000;
-const TEST_URL = `http://localhost:${PORT}`
+const TEST_URL = `http://localhost:${PORT}`;
 
 describe('TodoMVC', function() {
-  let page
-  let server
-  let browser
+  this.timeout(6000);
+  let page;
+  let server;
+  let browser;
 
-  before(() => {
-    server = httpServer.createServer({ root: `${__dirname}/..`})
-    server.listen(PORT)
-  });
+  before(async () => {
+    server = httpServer.createServer({ root: `${__dirname}/..` });
+    server.listen(PORT);
 
-  after(() => {
-    server.close()
-  });
-
-  beforeEach(async function() {
-    // Create a new Puppeteer browser instace for each test case
     browser = await puppeteer.launch({
       headless: true,
       timeout: 10000,
       args: puppeteerArgs
-    })
-    page = await browser.newPage()
-  })
+    });
+  });
+
+  after(() => {
+    server.close();
+  });
+
+  beforeEach(async function() {
+    page = await browser.newPage();
+    await page.goto(TEST_URL);
+    await page.evaluate(() => localStorage.clear());
+  });
 
   afterEach(function() {
-    // Close the Puppeteer browser instance.
-    browser.close()
-  })
+    page.close();
+  });
 
   it('Loads the app', async function() {
-    await page.goto(TEST_URL)
-    const mainContainer = await page.$('section.todoapp')
-    should.exist(mainContainer)
-    await percySnapshot(page, this.test.fullTitle())
-  })
+    const mainContainer = await page.$('section.todoapp');
+    should.exist(mainContainer);
+    await percySnapshot(page, this.test.fullTitle());
+  });
 
   it('With no todos, hides main section', async function() {
-    await page.goto(TEST_URL)
-    const hiddenMainSection = await page.$(".main[style*='display:none']")
-    should.exist(hiddenMainSection)
-  })
+    const hiddenMainSection = await page.$(".main[style*='display:none']");
+    should.exist(hiddenMainSection);
+  });
 
   it('Accepts a new todo', async function() {
-    await page.goto(TEST_URL)
-    await page.type('.new-todo', 'New fancy todo')
-    await page.keyboard.press('Enter')
-    const todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length)
-    todoCount.should.eq(1)
-    await percySnapshot(page, 'Snapshot with new todo', {widths: [300]})
-  })
+    await page.type('.new-todo', 'New fancy todo');
+    await page.keyboard.press('Enter');
+    const todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length);
+    todoCount.should.eq(1);
+    await percySnapshot(page, 'Snapshot with new todo', { widths: [300] });
+  });
 
   it('Lets you check off a todo', async function() {
-    await page.goto(TEST_URL)
-    await page.type('.new-todo', 'A thing to accomplish')
-    await page.keyboard.press('Enter')
+    await page.type('.new-todo', 'A thing to accomplish');
+    await page.keyboard.press('Enter');
 
-    let itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent)
-    itemsLeft.should.eq('1 item left')
+    let itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent);
+    itemsLeft.should.eq('1 item left');
 
-
-    await page.click('input.toggle')
-    itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent)
-    itemsLeft.should.eq('0 items left')
-    await percySnapshot(page, this.test.fullTitle(), {widths: [768, 992, 1200]})
-  })
+    await page.click('input.toggle');
+    itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent);
+    itemsLeft.should.eq('0 items left');
+    await percySnapshot(page, this.test.fullTitle(), { widths: [768, 992, 1200] });
+  });
 
   it('Demonstrates scoping snapshot to a selector', async function() {
     // Helper to scope a page to a selector
     async function scopePage(page, selector) {
       await page.evaluate(function(selector) {
         let scopedContainer = document.querySelector(selector);
-        document.querySelector('body').innerHTML = scopedContainer.outerHTML
-      }, selector)
+        document.querySelector('body').innerHTML = scopedContainer.outerHTML;
+      }, selector);
     }
 
-    await page.goto(TEST_URL)
-
     // Enter a todo, so that there is a todo list.
-    await page.type('.new-todo', 'New fancy todo')
-    await page.keyboard.press('Enter')
+    await page.type('.new-todo', 'New fancy todo');
+    await page.keyboard.press('Enter');
 
     // Scope the page to just the todo list and snapshot
-    await scopePage(page, '.main')
-    await percySnapshot(page, '.todo-list scoped snapshot')
-  })
-})
+    await scopePage(page, '.main');
+    await percySnapshot(page, '.todo-list scoped snapshot');
+  });
+});
