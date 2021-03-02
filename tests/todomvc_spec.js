d9a639bc870d5f65fcbd565dd56325268cd81b60
Robert DeLuca <robertdeluca19@gmail.com>
2021-03-02 16:42:18 -0600

    ✨ Upgrade to @percy/puppeteer v2 (#85)
    * :sparkles: Upgrade to v2 of @percy/puppeteer
    
    Upgraded by running `npx @percy/migrate` and following the prompts
    
    * 💚 Fix CI config (remove node 10)
    
    * 👷 Use GitHub actions for CI
    
    * ⬆️Upgrade the TodoMVC app bundled
    
    * :memo: Update README
    
    * ⬆️Update tests (and fix todo app bug)
    
    * :bug: Fix example app from replacing `innerHTML` on clear btn
    
    This prevents any changes from being made in `index.html` for this button.
    
    * 🔪 Remove `js/bundle.js` as it's never used
    
    * 🔪 Remove unneeded todomvc-common package
    
    * :memo: Update app to use inline style (not a class)

--- tests/todomvc_spec.js
+++ tests/todomvc_spec.js
@@ -1,14 +1,15 @@
-const httpServer = require('http-server');
-const should = require('chai').should();
+const expect = require('expect');
 const puppeteer = require('puppeteer');
-const { percySnapshot } = require('@percy/puppeteer');
+const httpServer = require('http-server');
+const percySnapshot = require('@percy/puppeteer');
+
 const platform = require('os').platform();
 // We need to change the args passed to puppeteer based on the platform they're using
 const puppeteerArgs = /^win/.test(platform) ? [] : ['--single-process'];
 const PORT = process.env.PORT_NUMBER || 8000;
 const TEST_URL = `http://localhost:${PORT}`;
 
-describe('TodoMVC', function() {
+describe('TodoMVC', function () {
   this.timeout(6000);
   let page;
   let server;
@@ -29,63 +30,50 @@ describe('TodoMVC', function() {
     server.close();
   });
 
-  beforeEach(async function() {
+  beforeEach(async function () {
     page = await browser.newPage();
     await page.goto(TEST_URL);
     await page.evaluate(() => localStorage.clear());
   });
 
-  afterEach(function() {
+  afterEach(function () {
     page.close();
   });
 
-  it('Loads the app', async function() {
-    const mainContainer = await page.$('section.todoapp');
-    should.exist(mainContainer);
+  it('Loads the app', async function () {
+    let mainContainer = await page.$('section.todoapp');
+    expect(mainContainer).toBeDefined();
+
     await percySnapshot(page, this.test.fullTitle());
   });
 
-  it('With no todos, hides main section', async function() {
-    const hiddenMainSection = await page.$(".main[style*='display:none']");
-    should.exist(hiddenMainSection);
+  it('With no todos, hides main section', async function () {
+    let display = await page.evaluate(() => document.querySelector('.main').style.display);
+
+    expect(display).toEqual('none');
   });
 
-  it('Accepts a new todo', async function() {
+  it('Accepts a new todo', async function () {
     await page.type('.new-todo', 'New fancy todo');
     await page.keyboard.press('Enter');
-    const todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length);
-    todoCount.should.eq(1);
+
+    let todoCount = await page.evaluate(() => document.querySelectorAll('.todo-list li').length);
+    expect(todoCount).toEqual(1);
+
     await percySnapshot(page, 'Snapshot with new todo', { widths: [300] });
   });
 
-  it('Lets you check off a todo', async function() {
+  it('Lets you check off a todo', async function () {
     await page.type('.new-todo', 'A thing to accomplish');
     await page.keyboard.press('Enter');
 
     let itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent);
-    itemsLeft.should.eq('1 item left');
+    expect(itemsLeft).toEqual('1 item left');
 
     await page.click('input.toggle');
     itemsLeft = await page.evaluate(() => document.querySelector('.todo-count').textContent);
-    itemsLeft.should.eq('0 items left');
+    expect(itemsLeft).toEqual('0 items left');
+
     await percySnapshot(page, this.test.fullTitle(), { widths: [768, 992, 1200] });
-  });
-
-  it('Demonstrates scoping snapshot to a selector', async function() {
-    // Helper to scope a page to a selector
-    async function scopePage(page, selector) {
-      await page.evaluate(function(selector) {
-        let scopedContainer = document.querySelector(selector);
-        document.querySelector('body').innerHTML = scopedContainer.outerHTML;
-      }, selector);
-    }
-
-    // Enter a todo, so that there is a todo list.
-    await page.type('.new-todo', 'New fancy todo');
-    await page.keyboard.press('Enter');
-
-    // Scope the page to just the todo list and snapshot
-    await scopePage(page, '.main');
-    await percySnapshot(page, '.todo-list scoped snapshot');
   });
 });
