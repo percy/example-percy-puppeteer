4c13427dfb2835b37da4caa33294bff63a40a3db
Pranav Z <pranav.z@browserstack.com>
2025-10-15 15:45:07 +0530

    chore: update dependencies and upgrade @percy/cli (#195)
    * chore: update dependencies and fix tests for new expect API; upgrade @percy/cli
    
    * chore(ci): use Node 18 and setup-node v3 in workflow
    
    * empty commit
    
    * resolve comment

--- tests/todomvc_spec.js
+++ tests/todomvc_spec.js
@@ -1,4 +1,4 @@
-const expect = require('expect');
+const expect = require('expect').default;
 const puppeteer = require('puppeteer');
 const httpServer = require('http-server');
 const percySnapshot = require('@percy/puppeteer');
